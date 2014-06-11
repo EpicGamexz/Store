56c75aa4561f892e2474d3e3d3cb127eefaaa57d
EpicGamexz <epicgamexz@gmail.com>
2014-06-11 17:13:47 -0500

    0.0.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Newstore::Application.config.secret_key_base = '2236db91848797c580f9ad5d8a618a2afa48aa363fbbbfa22fce5deabfc367fd61c896982059d34f30411b50017c2c9e919ef02347931ca79e3860492f778c2b'
